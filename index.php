47b86f479ac2f834d321db390f2a94ac5de91642
Eth Lny <cryptiareta@gmail.com>
2022-03-14 10:27:06 +0100

    svg

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Paint en mieux</title>
+    <link rel="stylesheet" href="./styles/styles.css" />
+    <meta http-equiv="refresh" content="30" />
+</head>
+
+<body>
+    <div class="tool-bar">
+        <div class="tools">
+            <div class="tool shapes circle "><img src="./styles/img/Circle.svg" /></div>
+            <div class="tool shapes triangle"><img src="./styles/img/Triangle.svg" /></div>
+            <div class="tool rectange"><img src="./styles/img/Vector-1.svg" /></div>
+            <div class="tool shapes txt"><img src="./styles/img/Vector.svg" /></div>
+            <div class="tool primary"><input type="color" name="primary-color" id=""></div>
+            <div class="tool secondary"><input type="color" name="secondary-color" id=""></div>
+            <div class="tool eraser"><img src="./styles/img/Erase.svg" /></div>
+            <div class="tool save"><img src="./styles/img/Vector-2.svg" /></div>
+        </div>
+    </div>
+    <div class="board"></div>
+</body>
+
+</html>
