9222160ec750a98f34f79183b9a351b23d7e5ff9
gaoubak <80465380+gaoubak@users.noreply.github.com>
2022-03-14 15:58:00 +0100

    modif-1 form and movable

--- js/form.js
+++ js/form.js
@@ -12,50 +12,35 @@ var img = document.createElement("img");
 
 function getForm() {
   var conteneur = document.getElementById("board");
-  if (document.getElementsById("circle").clicked == true) {
-    ma_Form = document.createElement("div");
-    img = document.createElement("img");
-    ma_Form.class = "circle";
-    ma_Form.id = new Date.now();
-    img.src = "./styles/img/Circle.svg";
-    ma_Form.appendChild("img");
-    conteneur.appendChild(ma_Form);
-  }
-  if (document.getElementsById("triangle").clicked == true) {
-    ma_Form = document.createElement("div");
-    img = document.createElement("img");
-    ma_Form.class = "triangle";
-    ma_Form.id = new Date.now();
-    img.src = "../styles/img/Triangle.svg";
-    ma_Form.appendChild("img");
-    conteneur.appendChild(ma_Form);
-  }
-  if (document.getElementsById("rectange").clicked == true) {
-    ma_Form = document.createElement("div");
-    img = document.createElement("img");
-    ma_Form.class = "rectange";
-    ma_Form.id = new Date.now();
-    img.src = "../styles/img/Rectangle.svg";
-    ma_Form.appendChild("img");
-    conteneur.appendChild(ma_Form);
-  }
-  if (document.getElementsById("txt").clicked == true) {
-    ma_Form = document.createElement("div");
-    img = document.createElement("img");
-    ma_Form.class = "tool shapes";
-    ma_Form.id = new Date.now();
-    img.src = "../styles/img/Txt.svg";
-    ma_Form.appendChild("img");
-    conteneur.appendChild(ma_Form);
-  }
-  if (document.getElementsById("txt").clicked == true) {
-    ma_Form = document.createElement("div");
-    img = document.createElement("img");
-    ma_Form.class = "tool shapes";
-    ma_Form.id = new Date.now();
-    img.src = "../styles/img/Txt.svg";
-    ma_Form.appendChild("img");
-    conteneur.appendChild(ma_Form);
+   // Create the Form for paint
+  function addShape($shape) {
+    if ($shape == "circle") {
+      ma_Form = document.createElement("div");
+      img = document.createElement("img");
+      ma_Form.class = "circle";
+      ma_Form.id = new Date.now();
+      img.src = "./styles/img/Circle.svg";
+      ma_Form.appendChild("img");
+      conteneur.appendChild(ma_Form);
+    }
+    if ($shape == "triangle") {
+      ma_Form = document.createElement("div");
+      img = document.createElement("img");
+      ma_Form.class = "triangle";
+      ma_Form.id = new Date.now();
+      img.src = "../styles/img/Triangle.svg";
+      ma_Form.appendChild("img");
+      conteneur.appendChild(ma_Form);
+    }
+    if ($shape == "rectangle") {
+      ma_Form = document.createElement("div");
+      img = document.createElement("img");
+      ma_Form.class = "rectangle";
+      ma_Form.id = new Date.now();
+      img.src = "../styles/img/Rectangle.svg";
+      ma_Form.appendChild("img");
+      conteneur.appendChild(ma_Form);
+    }
   }
   // cirle =;
   //Askip y a des balise html/svg qui permettent de faire les forms directement
