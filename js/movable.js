e73a8f3d980835b0738afd9d5caee0d9806d7607
Eth Lny <cryptiareta@gmail.com>
2022-03-14 15:03:27 +0100

    smol update

--- js/movable.js
+++ js/movable.js
@@ -14,10 +14,9 @@ function runDrag() {
         pos2 = 0,
         pos3 = 0,
         pos4 = 0;
-      if (document.getElementById(elmnt.id + "header")) {
-        // if present, the header is where you move the DIV from:
-        document.getElementById(elmnt.id + "header").onmousedown =
-          dragMouseDown;
+      if (document.getElementById(elmnt.id + "board")) {
+        // if present, the board is where you move the DIV from:
+        document.getElementById(elmnt.id + "board").onmousedown = dragMouseDown;
       } else {
         // otherwise, move the DIV from anywhere inside the DIV:
         elmnt.onmousedown = dragMouseDown;
@@ -51,6 +50,7 @@ function runDrag() {
         // stop moving when mouse button is released:
         document.onmouseup = null;
         document.onmousemove = null;
+        let draggableActive = false;
       }
     }
   }
